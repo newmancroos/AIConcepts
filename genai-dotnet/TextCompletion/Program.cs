b14e8163cc2b57388ddc7befb06cec5241164542
newancroos <newmancroos@gmail.com>
2026-04-28 23:08:47 -0400

    Text Completion

---
+++ genai-dotnet/TextCompletion/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using System.ClientModel;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var credential = new ApiKeyCredential(config["GitHubModels:Token"] ?? throw new ArgumentNullException("GitHubModels:Token"));
+
+var options = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.github.ai/inference")
+};
+
+//Create chat client 
+IChatClient client = new OpenAIClient(credential, options).GetChatClient("openai/gpt-4o-mini").AsIChatClient();
+
+
+#region Basic Completion
+////Send promt and get response
+
+////var response = await client.GetResponseAsync("What is AI? explain max 20 words.");
+////Console.WriteLine(response);
+////Console.ReadLine();
+
+//string prompt = "What is AI? explain max 20 words.";
+
+//Console.WriteLine($"User >>> {prompt}");
+
+//ChatResponse response = await client.GetResponseAsync(prompt);
+//Console.WriteLine($"AI >>> {response}");
+//Console.WriteLine($"Toekn Used: In= {response.Usage?.InputTokenCount}, Out= {response.Usage?.OutputTokenCount}");
+//Console.ReadLine();
+#endregion
+
+#region Streamimg
+
+string prompt = "What is AI? explain max 200 words.";
+
+Console.WriteLine($"User >>> {prompt}");
+
+var responseStream = client.GetStreamingResponseAsync(prompt);
+
+await foreach (var message in responseStream)
+{ 
+    Console.Write(message);
+}
+
+Console.ReadLine();
+
+#endregion
---
+++ genai-dotnet/TextCompletion/ReadMe.txt
@@ -0,0 +1,12 @@
+﻿- Install 
+	* Microsoft.Extensions.AI
+	* Microsoft.Extensions.AI.OpenAI   - Use to communicate with Azure OpenAI or OpenAI Models
+	* Microsoft.Extensions.Configuration
+	* Microsoft.Extensions.Configuration.UserSecrets
+
+- Right click the project and select "Manage User Secrets". This will open a secrets.json file 
+	where you can add your API keys and other sensitive information.
+	- Add the following configuration to the secrets.json file:
+		{
+			"GitHubModels:Token": "<Token here>"
+		}
