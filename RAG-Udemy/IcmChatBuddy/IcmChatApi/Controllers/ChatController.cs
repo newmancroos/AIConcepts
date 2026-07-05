89f2293bdffaea22f1e7887fa898a3460b498e94
newancroos <newmancroos@gmail.com>
2026-07-04 23:21:55 -0400

    Added RAG initial commit

---
+++ RAG-Udemy/IcmChatBuddy/IcmChatApi/Controllers/ChatController.cs
@@ -0,0 +1,35 @@
+﻿using IcmChatApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.AI;
+
+namespace IcmChatApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ChatController : ControllerBase
+{
+    private readonly IChatClient _chatClient;
+    public ChatController(IChatClient chatClient)
+    {
+        _chatClient = chatClient;
+    }
+
+    [HttpPost]
+    public async Task<Models.ChatResponse> SendQuery([FromBody] ChatRequest query)
+    {
+        List<ChatMessage> messages = new List<ChatMessage>()
+        {
+            new ChatMessage(ChatRole.System, "You are a general Chat system"),
+            new ChatMessage(ChatRole.User, query.Query)
+        };
+
+        var response =await  _chatClient.GetResponseAsync(messages, new ChatOptions());
+
+        return new Models.ChatResponse
+        {
+            Message = response.Text,
+            Status = "Success"
+        };
+    }
+
+}
---
+++ RAG-Udemy/IcmChatBuddy/IcmChatBuddy.AppHost/AppHost.cs
@@ -0,0 +1,23 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var ollama = builder.AddOllama("ollama")
+    .WithDataVolume();
+
+var chatModel = ollama.AddModel("chat", "llama3.2");
+
+
+//Testing Ollama-lamma3.2 model, we can use GithubWebUi
+
+//builder.AddContainer("open-webui", "ghcr.io/open-webui/open-webui", "main")
+//    .WithHttpEndpoint(port: 3000, targetPort: 8080, name: "http")
+//    .WithEnvironment("OLLAMA_BASE_URL", ollama.GetEndpoint("http"))
+//    .WithLifetime(ContainerLifetime.Persistent)
+//    .WaitFor(ollama);
+
+builder.AddProject<Projects.IcmChatApi>("icmchatapi")
+    .WithReference(chatModel)
+    .WaitFor(chatModel);
+
+builder.AddProject<Projects.IngestionService>("ingestionservice");
+
+builder.Build().Run();
